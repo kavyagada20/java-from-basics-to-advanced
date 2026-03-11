1fe18ec369039b09423e75ce9603098d6efdb74b
kavya <kavyagada20@gmail.com>
2026-03-11 15:20:45 +0530

    Add loop programs

---
+++ Loops/For.java
@@ -0,0 +1,13 @@
+public class For {
+    public static void main(String[] args) {
+        // Basic syntax of for loops
+        // for(intilization;conditions;updations)
+        // {
+        //     conditions;
+        // }
+        for(int counter=0;counter<3;counter++)
+        {
+            System.out.println("hello world");
+        }
+    }
+}
---
+++ Loops/Loops.java
@@ -0,0 +1,9 @@
+public class Loops{
+ 	public static void main(String args[]){
+		for(int counter=0;counter<11;counter++)
+	//counter++ is counter=counter+1
+			{
+			System.out.println(counter);
+			}
+		}
+}
